7b0e05afd1c98291098a172520c15edfea03c83b
hamida-mstafa <hamidamstafa@gmail.com>
2018-10-22 11:18:27 +0300

    introduced quotes.ts

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
-// export class Quote {
-//   public showDescription:boolean;
-// constructor(public id:number, public name:string, public author:string, public user:string, public upVote:number,){
-//   this.showDescription=false
-// }
-// }
+export class Quote {
+  public showDescription:boolean;
+constructor(public id:number, public name:string, public author:string, public user:string, public upVote:number,){
+  this.showDescription=false
+}
+}
