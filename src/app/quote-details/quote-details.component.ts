81fdd8b5fae4c3d0532493bc5e28ebd8d00e93d0
hamida-mstafa <hamidamstafa@gmail.com>
2018-10-22 11:13:02 +0300

    modified app component

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,27 +1,27 @@
-// import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
-// import {Quote} from '../quotes'
-// @Component({
-//   selector: 'app-quote-details',
-//   templateUrl: './quote-details.component.html',
-//   styleUrls: ['./quote-details.component.css']
-// })
-// export class QuoteDetailsComponent implements OnInit {
-//   @Input() quote:Quote;
-//   @Output() isComplete= new EventEmitter<boolean>();
-//
-//   quoteDelete(complete:boolean){
-//   this.isComplete.emit(complete);
-// }
-//   upVote() {
-//         this.upVote ++
-//       }
-//
-//       downVote() {
-//         this.upVote --
-//       }
-//   constructor() { }
-//
-//   ngOnInit() {
-//   }
-//
-// }
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quotes'
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+  @Input() quote:Quote;
+  @Output() isComplete= new EventEmitter<boolean>();
+
+  quoteDelete(complete:boolean){
+  this.isComplete.emit(complete);
+}
+  upVote() {
+        this.upVote ++
+      }
+
+      downVote() {
+        this.upVote --
+      }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
