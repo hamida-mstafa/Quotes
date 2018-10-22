c148eba0680a6911430cac7f36c2df4b4a4da50f
hamida-mstafa <hamidamstafa@gmail.com>
2018-10-22 11:13:47 +0300

    modified app component .ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-// import { Component } from '@angular/core';
-// import {Quote} from './quotes';
-//
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = '';
-//
-// }
+import { Component } from '@angular/core';
+import {Quote} from './quotes';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '';
+
+}
